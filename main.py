e7fb49222b3fb8a4f8f14fcc3112b16dbf4ab2fd
Aimee <aimee4312@gmail.com>
2023-10-08 23:17:46 -0700

    added the add movie and show commands

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ import discord
 import requests
 import json
 import random
+import pymongo
+
 from config import TOKEN
+mongo_client = pymongo.MongoClient("mongodb+srv://billiam:1fVVhMFspTHjx64E@cluster0.d15oseu.mongodb.net/")
 
 intents = discord.Intents.all() 
 client = discord.Client(command_prefix='!', intents=intents)
@@ -37,6 +40,35 @@ async def on_message(message):
     
     if any(word in msg for word in sad_words):
         await message.channel.send(random.choice(starter_encouragements))
+    
+    if msg.startswith('!add-movie'):
+        movie_name = msg.split('!add-movie', 1)[1].strip()
 
+        db = mongo_client['Unwatched']
+        collection = db['Movie']
+
+        movie_document = {
+            "name": movie_name,
+            "watched": False
+        }
+
+        collection.insert_one(movie_document)
+
+        await message.channel.send(f"Added '{movie_name}' to the movie list.")
+
+    if msg.startswith('!add-show'):
+        show_name = msg.split('!add-show', 1)[1].strip()
+
+        db = mongo_client['Unwatched']
+        collection = db['Show']
+
+        show_document = {
+            "name": show_name,
+            "watched": False
+        }
+
+        collection.insert_one(show_document)
+
+        await message.channel.send(f"Added '{show_name}' to the show list.")
     
 client.run(TOKEN)
