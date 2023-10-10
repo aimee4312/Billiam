7dd5779bf82295cd77f18a63952aa28dbb545a64
Aimee <aimee4312@gmail.com>
2023-10-10 00:02:37 -0700

    added get and list commands

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ db = mongo_billiam['Watchlist']
 async def on_ready():
     print('We have logged in as {0.user}'.format(billiam))
 
+# /add-movie
 @slash_command(
     name="add-movie",
     description="Add a movie to the list",
@@ -35,6 +36,7 @@ async def add_movie(ctx, movie: str):
     collection.insert_one(movie_document)
     await ctx.respond(f"Added '{movie}' to the movie list.")
 
+# /add-show
 @slash_command(
     name="add-show",
     description="Add a show to the list",
@@ -57,6 +59,7 @@ async def add_show(ctx, show: str):
     collection.insert_one(show_document)
     await ctx.respond(f"Added '{show}' to the show list.")
 
+# /get-movie
 @slash_command(
     name="get-movie",
     description="Get a random movie off the list",
@@ -71,6 +74,91 @@ async def get_movie(ctx):
         random_movie = random.choice(unwatched_movies)
         collection.update_one({"_id": random_movie["_id"]}, {"$set": {"watched": True}})
         await ctx.respond(f"Random Movie: {random_movie['name']}")
+
+# /get-show
+@slash_command(
+    name="get-show",
+    description="Get a random show off the list",
+)
+async def get_show(ctx):
+    collection = db['Shows']
+
+    unwatched_shows = list(collection.find({"watched": False}))
+    if not unwatched_shows:
+        await ctx.respond("There are no unwateched shows in the list.")
+    else:
+        random_show = random.choice(unwatched_shows)
+        collection.update_one({"_id": random_show["_id"]}, {"$set": {"watched": True}})
+        await ctx.respond(f"Random Show: {random_show['name']}")
+
+# /viewed-movies
+@slash_command(
+    name="viewed-movies",
+    description="Display list of viewed movies",
+)
+async def viewed_movies(ctx):
+    collection = db['Movies']
+
+    watched_movies = list(collection.find({"watched": True}))
+    num_movies = collection.count_documents({"watched": True})
+    if num_movies == 0:
+        await ctx.respond("There are no movies watched.")
+    else:
+        
+        movie_list = "\n".join([f"{i + 1}. {movie['name']}" for i, movie in enumerate(watched_movies)])
+        await ctx.respond("List of Watched Movies:\n" + movie_list)
+
+# /viewed-shows
+@slash_command(
+    name="viewed-shows",
+    description="Display list of viewed shows",
+)
+async def viewed_shows(ctx):
+    collection = db['Shows']
+
+    watched_shows = list(collection.find({"watched": True}))
+    num_shows = collection.count_documents({"watched": True})
+    if num_shows == 0:
+        await ctx.respond("There are no shows watched.")
+    else:
+        
+        shows_list = "\n".join([f"{i + 1}. {show['name']}" for i, show in enumerate(watched_shows)])
+        await ctx.respond("List of Watched Shows:\n" + shows_list)
+
+# /unviewed-movies
+@slash_command(
+    name="unviewed-movies",
+    description="Display list of unviewed movies",
+)
+async def unviewed_movies(ctx):
+    collection = db['Movies']
+
+    unwatched_movies = list(collection.find({"watched": False}))
+    num_movies = collection.count_documents({"watched": False})
+    if num_movies == 0:
+        await ctx.respond("There are no movies unwatched.")
+    else:
+        
+        movie_list = "\n".join([f"{i + 1}. {movie['name']}" for i, movie in enumerate(unwatched_movies)])
+        await ctx.respond("List of Unwatched Movies:\n" + movie_list)
+
+# /unviewed-shows
+@slash_command(
+    name="unviewed-shows",
+    description="Display list of unviewed shows",
+)
+async def unviewed_shows(ctx):
+    collection = db['Shows']
+
+    unwatched_shows = list(collection.find({"watched": False}))
+    num_shows = collection.count_documents({"watched": False})
+    if num_shows == 0:
+        await ctx.respond("There are no shows unwatched.")
+    else:
+        
+        shows_list = "\n".join([f"{i + 1}. {show['name']}" for i, show in enumerate(unwatched_shows)])
+        await ctx.respond("List of Unwatched Shows:\n" + shows_list)
+
 @billiam.event
 async def on_message(message):
     if message.author == billiam.user:
