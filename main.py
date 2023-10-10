febf88e7814231525e10f933220f875d77db9d9d
Aimee <aimee4312@gmail.com>
2023-10-09 23:27:55 -0700

    revamped it to be implemented as slash commands

--- main.py
+++ main.py
@@ -1,118 +1,44 @@
-import discord
-import requests
-import json
-import random
 import pymongo
+import interactions
 
-from config import TOKEN
-from discord.ext import commands
+from interactions import slash_command
+from config import TOKEN, MONGODB
 
-mongo_client = pymongo.MongoClient("mongodb+srv://billiam:1fVVhMFspTHjx64E@cluster0.d15oseu.mongodb.net/")
+billiam = interactions.Client(token=TOKEN)
+mongo_billiam = pymongo.MongoClient(MONGODB)
+db = mongo_billiam['Watchlist']
 
-intents = discord.Intents.all() 
-client = discord.Client(command_prefix='!', intents=intents)
-db = mongo_client['Watchlist']
-
-sad_words = ["sad", "depressed", "unhappy", "angry", "miserable"]
-starter_encouragements = [
-  "Cheer up!",
-  "Hang in there.",
-  "You are a great person / bot!"
-]
-
-def get_quote():
-    response = requests.get("https://zenquotes.io/api/random")
-    json_data = json.loads(response.text)
-    quote = json_data[0]['q'] + " -" + json_data[0]['a']
-    return(quote)
-
-@client.event
+@billiam.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print('We have logged in as {0.user}'.format(billiam))
 
-@client.event
+@slash_command(
+    name="add-movie",
+    description="Add a movie to the list",
+    options=[
+        {
+            "name": "movie_name",
+            "description": "The name of the movie",
+            "type": 3,
+            "required": True,
+        }
+    ]
+)
+async def add_movie(ctx, movie_name: str):
+    collection = db['Movies']
+
+    movie_document = {
+        "name": movie_name,
+        "watched": False
+    }
+    collection.insert_one(movie_document)
+    await ctx.respond(f"Added '{movie_name}' to the movie list.")
+
+@billiam.event
 async def on_message(message):
-    if message.author == client.user:
+    if message.author == billiam.user:
         return
-    
-    msg = message.content
-
-    # Random inspo
-    if msg.startswith('!inspire'):
-        quote = get_quote()
-        await message.channel.send(quote)
-    
-    # Random encouragement
-    if any(word in msg for word in sad_words):
-        await message.channel.send(random.choice(starter_encouragements))
-    
-    # Add movie
-    if msg.startswith('!add-movie'):
-        movie_name = msg.split('!add-movie', 1)[1].strip()
-        collection = db['Movie']
-
-        movie_document = {
-            "name": movie_name,
-            "watched": False
-        }
-
-        collection.insert_one(movie_document)
-
-        await message.channel.send(f"Added '{movie_name}' to the movie list.")
-
-    # Add show
-    if msg.startswith('!add-show'):
-        show_name = msg.split('!add-show', 1)[1].strip()
-        collection = db['Show']
-
-        show_document = {
-            "name": show_name,
-            "watched": False
-        }
-
-        collection.insert_one(show_document)
-
-        await message.channel.send(f"Added '{show_name}' to the show list.")
-    
-     # See movie list
-    if msg.startswith('!list-movies'):
-        collection = db['Movie']
-        movies = collection.find({"watched": False})
-
-        num_movies = collection.count_documents({"watched": False})
-        if num_movies == 0:
-            await message.channel.send("There are no unwatched movies in the list.")
-        else:
-            # Create a numbered list of shows
-            movie_list = "\n".join([f"{i + 1}. {movie['name']}" for i, movie in enumerate(movies)])
-
-            await message.channel.send("List of unwatched movies:\n" + movie_list)
-
-    # See show list
-    if msg.startswith('!list-shows'):
-        collection = db['Show']
-        shows = collection.find({"watched": False})
-
-        num_shows = collection.count_documents({"watched": False})
-        if num_shows == 0:
-            await message.channel.send("There are no unwatched shows in the list.")
-        else:
-            # Create a numbered list of shows
-            show_list = "\n".join([f"{i + 1}. {show['name']}" for i, show in enumerate(shows)])
-
-            await message.channel.send("List of Unwatched Shows:\n" + show_list)
-    
-    # Get a random movie
-    if msg.startswith('!get-movie'):
-        collection = db['Movie']
-
-        unwatched_movies = list(collection.find({"watched": False}))
-        if not unwatched_movies:
-            await message.channel.send("There are no unwatched movies in the list.")
-        else:
-            random_movie = random.choice(unwatched_movies)
-            collection.update_one({"_id": random_movie["_id"]}, {"$set": {"watched": True}})
-            await message.channel.send(f"Random Movie: {random_movie['name']}")
+    await billiam.process_commands(message)
 
 
-client.run(TOKEN)
+billiam.start()
