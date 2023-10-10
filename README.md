9ae101288921975df703e11a4d02566e41df40af
Aimee <aimee4312@gmail.com>
2023-10-09 23:50:04 -0700

    added more commands + updated readme

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
-# Billiam
+# Billiam #
 
 A Discord bot for movie recommendations, implementing randomization and list management for watched and unwatched films.
+
+| Slash Command     | Description                       |
+| ----------------- | --------------------------------- |
+| /add-movie        | Adds a movie to the unviewed movie list  |
+| /add-show         | Adds a show to the unviewed show list    |
+| /get-movie        | Gets a random movie from the unviewed movie list |
+| /get-show         | Gets a random show from the unviewed show list  |
+| /viewed-movies    | Displays viewed movies               |
+| /viewed-shows     | Displays viewed shows                |
+| /unviewed-movies  | Displays unviewed movies            |
+| /unviewed-shows   | Displays unviewed shows             |
+| /remove-movie     | Removes movie from all lists         |
+| /remove-show      | Removes show from all lists          |
+
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pymongo
 import interactions
+import random
 
 from interactions import slash_command
 from config import TOKEN, MONGODB
@@ -17,23 +18,59 @@ async def on_ready():
     description="Add a movie to the list",
     options=[
         {
-            "name": "movie_name",
+            "name": "movie",
             "description": "The name of the movie",
             "type": 3,
             "required": True,
         }
     ]
 )
-async def add_movie(ctx, movie_name: str):
+async def add_movie(ctx, movie: str):
     collection = db['Movies']
 
     movie_document = {
-        "name": movie_name,
+        "name": movie,
         "watched": False
     }
     collection.insert_one(movie_document)
-    await ctx.respond(f"Added '{movie_name}' to the movie list.")
+    await ctx.respond(f"Added '{movie}' to the movie list.")
 
+@slash_command(
+    name="add-show",
+    description="Add a show to the list",
+    options=[
+        {
+            "name": "show",
+            "description": "The name of the show",
+            "type": 3,
+            "required": True,
+        }
+    ]
+)
+async def add_show(ctx, show: str):
+    collection = db['Shows']
+
+    show_document = {
+        "name": show,
+        "watched": False
+    }
+    collection.insert_one(show_document)
+    await ctx.respond(f"Added '{show}' to the show list.")
+
+@slash_command(
+    name="get-movie",
+    description="Get a random movie off the list",
+)
+async def get_movie(ctx):
+    collection = db['Movies']
+
+    unwatched_movies = list(collection.find({"watched": False}))
+    if not unwatched_movies:
+        await ctx.respond("There are no unwateched movies in the list.")
+    else:
+        random_movie = random.choice(unwatched_movies)
+        collection.update_one({"_id": random_movie["_id"]}, {"$set": {"watched": True}})
+        await ctx.respond(f"Random Movie: {random_movie['name']}")
 @billiam.event
 async def on_message(message):
     if message.author == billiam.user:
